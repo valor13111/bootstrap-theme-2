ed1f6f53efece08c0ccc94070bfed91aed159c53
Tyler Reed <tyler.reed@ibm.com>
2018-03-20 15:33:06 -0400

    Added dev folders

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,55 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
-var ejs         = require('gulp-ejs');
+const gulp        = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass        = require('gulp-sass');
+const ejs         = require('gulp-ejs');
+const imagemin    = require('gulp-imagemin');
+const cleanCSS    = require('gulp-clean-css');
+const htmlmin     = require('gulp-htmlmin');
 
-gulp.task('sass', function() {
-  return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'scss/*.scss'])
+gulp.task('sass', () => {
+  return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', '_dev_stylesheets/scss/*.scss'])
     .pipe(sass())
-    .pipe(gulp.dest("dist/assets/css"))
+    .pipe(gulp.dest("_dev_stylesheets/css"))
     .pipe(browserSync.stream());
 });
 
-gulp.task('js', function() {
+gulp.task('js', () => {
   return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
     .pipe(gulp.dest("dist/assets/js"))
     .pipe(browserSync.stream());
 });
 
-gulp.task('ejs', function() {
+// TEMPLATING
+gulp.task('ejs', () => {
   return gulp.src('templates/*.ejs')
     .pipe(ejs({}, {}, { ext: '.html' }))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('_dev_html'));
 });
 
-gulp.task('server', ['sass'], function() {
+// MINIFYING TASKS
+gulp.task('imagemin', () => {
+  return gulp.src('_dev_images/*')
+    .pipe(imagemin({verbose: true}))
+    .pipe(gulp.dest('dist/assets/img'));
+});
+
+gulp.task('minify-css', () => {
+  return gulp.src('_dev_stylesheets/css/*.css')
+    .pipe(cleanCSS({debug: true}, (details) => {
+      console.log(`${details.name}: ${details.stats.originalSize}`);
+      console.log(`${details.name}: ${details.stats.minifiedSize}`);
+    }))
+    .pipe(gulp.dest('dist/assets/css'));
+});
+
+gulp.task('minify-html', function() {
+  return gulp.src('_dev_html/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist'));
+});
+
+// BROWSERSYNC
+gulp.task('server', ['sass'], () => {
   browserSync.init({
     server: "./dist"
   });
@@ -31,4 +58,5 @@ gulp.task('server', ['sass'], function() {
   gulp.watch("./dist/*.html").on('change', browserSync.reload);
 })
 
-gulp.task('default', ['js', 'server', 'ejs']);
+// DEFAULT TASK GULP EXECUTES
+gulp.task('default', ['js', 'server', 'ejs', 'imagemin', 'minify-css', 'minify-html']);
